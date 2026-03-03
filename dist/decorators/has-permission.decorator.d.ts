7c4d722e2249b65c002edae809922beb587ca499
alexiworld <alexiworld@yahoo.com>
2026-03-02 23:01:19 -0800

    Remove dist directory from repository and add to .gitignore

--- dist/decorators/has-permission.decorator.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const HAS_PERMISSION_KEY = "hasPermission";
-export declare const HasPermission: (permission: string) => import("@nestjs/common").CustomDecorator<string>;
-//# sourceMappingURL=has-permission.decorator.d.ts.map
